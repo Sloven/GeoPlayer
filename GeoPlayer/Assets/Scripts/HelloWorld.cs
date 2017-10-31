e84f02944f89ecc2d4b11c5a6bf9c7a893aecb55
Sloven <rubanvladimir@gmail.com>
2017-10-31 16:54:42 +0200

    First change test

--- GeoPlayer/Assets/Scripts/HelloWorld.cs
+++ GeoPlayer/Assets/Scripts/HelloWorld.cs
@@ -6,7 +6,7 @@ public class HelloWorld : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("Hello world log called");
 	}
 	
 	// Update is called once per frame
